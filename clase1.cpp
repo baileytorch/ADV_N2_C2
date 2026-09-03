ceee5fdda929a3b110f84d36a8d2a229d12b944b
BaileyTorch <baileytorch@hotmail.com>
2026-09-03 18:28:07 -0400

    Entrando al área 51

--- clase1.cpp
+++ clase1.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 using namespace std;
 
-string personajeVivo(int vida){
+string personajeVivo(int vida)
+{
     string respuesta = "";
-    if(vida <= 0){
+    if (vida <= 0)
+    {
         respuesta = "No";
-    }else{
+    }
+    else
+    {
         respuesta = "Si";
     }
     return respuesta;
 }
 
-string mostrarFicha(string nombre, int vida, int coins, float speed, bool llave){
+string mostrarFicha(string nombre, int vida, int nivel, int coins, float speed, bool llave)
+{
     cout << "" << endl;
     cout << "Ficha Tecnica" << endl;
     cout << "=============" << endl;
     cout << "Nombre: " << nombre << endl;
     cout << "Vida: " << vida << endl;
+    cout << "Nivel: " << nivel << endl;
     cout << "Monedas: " << coins << endl;
     cout << "Velocidad: " << speed << endl;
     cout << "Esta Vivo?: " << personajeVivo(vida) << endl;
@@ -24,7 +30,20 @@ string mostrarFicha(string nombre, int vida, int coins, float speed, bool llave)
     cout << "" << endl;
 }
 
-int main(){
+string accesoArea51(int nivel, bool llave)
+{
+    if (nivel >= 5 && llave == true)
+    {
+        cout << "Ha ingresado al Area 51!" << endl;
+    }
+    else
+    {
+        cout << "Los secretos del Area 51 siguen ocultos para Ud." << endl;
+    }
+}
+
+int main()
+{
     string nombrePersonaje = "Aquiles Baeza";
     int vida = 500;
     int monedas = 0;
@@ -33,31 +52,23 @@ int main(){
     bool tieneLlave = false;
     int nivel = 1;
 
-    mostrarFicha(nombrePersonaje,vida,monedas,velocidad,tieneLlave);
+    mostrarFicha(nombrePersonaje, vida, nivel, monedas, velocidad, tieneLlave);
 
     cout << "Tratando de ingresar al Area 51..." << endl;
     cout << "El nivel requerido es 5 y necesita una llave..." << endl;
+    accesoArea51(nivel,tieneLlave);
 
-    if (nivel >= 5 && tieneLlave == true)
-    {
-        cout << "Ha ingresado al Area 51!" <<endl;
-    }else{
-        cout << "Los secretos del Area 51 siguen ocultos para Ud." << endl;
-    }    
-
+    cout << "" << endl;
     cout << "Atacamos a nuestro personaje con 100 de danio!" << endl;
     vida = vida - 100;
     tieneLlave = true;
+    
+    cout << "" << endl;
     cout << "Por haber encontrado la llave, has subido 4 niveles!" << endl;
     nivel = nivel + 4;
 
-    if (nivel >= 5 && tieneLlave == true)
-    {
-        cout << "Ha ingresado al Area 51!" <<endl;
-    }else{
-        cout << "Los secretos del Area 51 siguen ocultos para Ud." << endl;
-    }  
+    mostrarFicha(nombrePersonaje, vida, nivel, monedas, velocidad, tieneLlave);
+    accesoArea51(nivel,tieneLlave);
 
-    mostrarFicha(nombrePersonaje,vida,monedas,velocidad,tieneLlave);
     return 0;
 }
